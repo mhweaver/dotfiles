4911be9030af96c95da14fde6d24670a166b43c9
mweaver <mweaver@clearwateranalytics.com>
2015-09-18 13:06:01 -0600

    Added link_config function to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,15 +4,21 @@ if [ ! -d ~/.config ]; then
 	mkdir ~/.config
 fi
 
+function link_config {
+	SRC_CONFIG_DIR=$1 # The config dir in this repo (e.g., fish)
+	CONFIG_DIR=$2 # The where the links should be
+	if [ -d $CONFIF_DIR ]; then
+		rm -r ${CONFIG_DIR}.bak || true
+		mv $CONFIG_DIR ${CONFIG_DIR}.bak
+		mkdir $CONFIG_DIR
+	fi
+
+	for link in $(ls $SRC_CONFIG_DIR); do
+		ln -s ${SRC_CONFIG_DIR}/${link} ${CONFIG_DIR}/${link}
+	done
+}
+
 # Link fish config
 FISH_DIR=$(pwd)/fish
-if [ -d ~/.config/fish ]; then
-	# If the dir already exists, make a backup
-	rm -r ~/.config/fish.bak || true
-	mv ~/.config/fish ~/.config/fish.bak
-	mkdir ~/.config/fish
-fi
-# Link everything in $FISH_DIR to ~/.config/fish
-for link in $(ls $FISH_DIR); do
-	ln -s ${FISH_DIR}/${link} ~/.config/fish/${link}
-done
+FISH_CONFIG_DIR=~/.config/fish
+link_config $FISH_DIR $FISH_CONFIG_DIR
