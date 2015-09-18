ab4d5d4d0d5db423227afde89d690ad941f7e6e2
mweaver <mweaver@clearwateranalytics.com>
2015-09-18 14:16:05 -0600

    Added tmuxinator to setup.sh

--- setup.sh
+++ setup.sh
@@ -33,3 +33,8 @@ TMUX_CONFIG=$(pwd)/tmux.conf
 TMUX_CONFIG_DIR=~
 rm -f ${TMUX_CONFIG_DIR}/.tmux.conf
 ln -s $TMUX_CONFIG $TMUX_CONFIG_DIR/.tmux.conf
+
+# Link tmuxinator config
+TMUXINATOR_CONFIG=$(pwd)/tmuxinator
+TMUXINATOR_CONFIG_DIR=~/.tmuxinator
+link_config $TMUXINATOR_CONFIG $TMUXINATOR_CONFIG_DIR
