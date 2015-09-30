4aa2f34e474c9c54f957ff1a9ac5d5601dfda8ab
mweaver <mweaver@clearwateranalytics.com>
2015-09-30 10:17:24 -0600

    Changed paths from ~/configs to ~/dotfiles to reflect the repo name

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 set -x EDITOR /usr/bin/vim
 set -x SHELL /usr/bin/fish
 # set fish_function_path $fish_function_path "/usr/local/lib/python2.7/dist-packages/powerline/bindings/fish"
-set -x PATH ~/configs/.extern/powerline/scripts $PATH
-#set -x POWERLINE_COMMAND ~/configs/.extern/powerline/scripts/powerline-daemon
-set fish_function_path $fish_function_path "~/configs/.extern/powerline/powerline/bindings/fish"
-source ~/configs/.extern/powerline/powerline/bindings/fish/powerline-setup.fish
+set -x PATH ~/dotfiles/.extern/powerline/scripts $PATH
+#set -x POWERLINE_COMMAND ~/dotfiles/.extern/powerline/scripts/powerline-daemon
+set fish_function_path $fish_function_path "~/dotfiles/.extern/powerline/powerline/bindings/fish"
+source ~/dotfiles/.extern/powerline/powerline/bindings/fish/powerline-setup.fish
 powerline-setup
