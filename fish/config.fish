da7fe1dc8e22e096f074c08f493815a4c6252ff7
mweaver <mweaver@clearwateranalytics.com>
2015-10-05 16:24:35 -0600

    Added PYTHONPATH and TERM to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,9 @@
+set -x TERM xterm-256color
 set -x EDITOR /usr/bin/vim
 set -x SHELL /usr/bin/fish
 # set fish_function_path $fish_function_path "/usr/local/lib/python2.7/dist-packages/powerline/bindings/fish"
 set -x PATH ~/dotfiles/.extern/powerline/scripts $PATH
+set -x PYTHONPATH ~/dotfiles/.extern/powerline/:$PYTHONPATH
 #set -x POWERLINE_COMMAND ~/dotfiles/.extern/powerline/scripts/powerline-daemon
 set fish_function_path $fish_function_path "~/dotfiles/.extern/powerline/powerline/bindings/fish"
 source ~/dotfiles/.extern/powerline/powerline/bindings/fish/powerline-setup.fish
