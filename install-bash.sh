fa267a5a08f80a773dc83b61edbc9f5028a84d2c
mweaver <mweaver@clearwateranalytics.com>
2016-02-12 13:37:27 -0700

    Removed unecessary \n's in install-bash

--- install-bash.sh
+++ install-bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ -z ${DOTFILES_DIR+x} ]; then
-	echo "\nsource $PWD/env.sh" >> $HOME/.bashrc
-	echo "\nsource $PWD/env.sh" >> $HOME/.profile
+	echo "source $PWD/env.sh" >> $HOME/.bashrc
+	echo "source $PWD/env.sh" >> $HOME/.profile
 fi
