3c50bf6e2fa147384cd448a8e2c56a9ca07e50ce
mhweaver <mhweaver@gmail.com>
2019-02-12 10:46:13 -0700

    Added vis config

---
+++ vis/.config/vis/visrc.lua
@@ -0,0 +1,16 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- Your global configuration options
+	vis:command('set theme solarized')
+	vis:command('set number')
+	vis:command('set tabwidth 4')
+	vis:command('set autoindent')
+	vis:command('set syntax')
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	-- Your per window configuration options e.g.
+	-- vis:command('set number')
+end)
